a6233abae9c09bcb77e897d3a868e25d504643a4
James Sergal <jamesthesergal@gmail.com>
2025-02-16 09:46:06 -0500

    Fix compile error

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -8,6 +8,7 @@ lazy_static!{
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -17,7 +18,13 @@ pub fn init_idt(){
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: InterruptStackFrame
-){
-    println!("EXCEPTION: Breakpoint\n{:#?}", stack_frame)
+    stack_frame: InterruptStackFrame)
+{
+    println!("EXCEPTION: Breakpoint\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64) -> !
+{
+    panic!("DOUBLE FAULT\n{:#?}", stack_frame)
 }
