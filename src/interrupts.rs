c0c866d20f6c221dc7957fcc34adbd6978455d9a
James Sergal <jamesthesergal@gmail.com>
2025-02-16 11:47:27 -0500

    Setup for system timer

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,35 @@
 use crate::println;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-
 use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use pic8259::ChainedPics;
+use spin;
+
+
+// Establish offsets for our PIC because the default lines overlap with our 
+// Inturrupts
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum PICInterruptIndex{
+    Timer = PIC_1_OFFSET, // Timer is on Line 0
+    Keyboard = PIC_1_OFFSET + 1
+}
+
+impl PICInterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)});
+
 
 
 lazy_static!{
@@ -15,6 +43,12 @@ lazy_static!{
 
 pub fn init_idt(){
     IDT.load();
+}
+
+extern "x86-interrupt" fn system_timer(
+    stack_frame: InterruptStackFrame)
+{
+    println!("EXCEPTION: Breakpoint\n{:#?}", stack_frame);
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,6 @@ pub mod interrupts;
 
 pub fn init(){
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize()};
+    x86_64::instructions::interrupts::enable();
 }
